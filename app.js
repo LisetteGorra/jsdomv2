24ffe8f074212dcc5d07817b90a2d4263a721e53
Lisette Gorra <lisettegorra.student@careerdevs.com>
2018-12-18 00:57:55 +0000

    Used Array.isArray, Array.from Array.from(titles).forEach

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
-const wmf = document.querySelector('#book-list li:nth-child(2) .name');
-console.log(wmf);
+var titles = document.getElementsByClassName('item');
+// console.log(Array.isArray(titles));
 
-var books = document.querySelector('#book-list li .name');
-console.log(books);
 
-books = document.querySelectorAll('#book-list li .name');
-console.log(books);
+Array.from(titles).forEach(function(item){
+//     console.log(item);
+// });
 
-Array.from(books).forEach(function(book){
-  console.log(book);
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+// console.log(wmf);
+
+// var books = document.querySelector('#book-list li .name');
+// console.log(books);
+
+// books = document.querySelectorAll('#book-list li .name');
+// console.log(books);
+
+// Array.from(books).forEach(function(book){
+//   console.log(book);
+// });
