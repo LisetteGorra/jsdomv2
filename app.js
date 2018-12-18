560d850d71349dc22f962bc826afc119dbafc88a
Lisette Gorra <lisettegorra.student@careerdevs.com>
2018-12-18 01:08:08 +0000

    version4.01

--- app.js
+++ app.js
@@ -1,38 +1,12 @@
-var titles = document.getElementsByClassName('item');
-// console.log(Array.isArray(titles));
+const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+console.log(wmf);
 
+var books = document.querySelector('#book-list li .name');
+console.log(books);
 
-Array.from(titles).forEach(function(item){
-//     console.log(item);
-// });
+books = document.querySelectorAll('#book-list li .name');
+console.log(books);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const wmf = document.querySelector('#book-list li:nth-child(2) .name');
-// console.log(wmf);
-
-// var books = document.querySelector('#book-list li .name');
-// console.log(books);
-
-// books = document.querySelectorAll('#book-list li .name');
-// console.log(books);
-
-// Array.from(books).forEach(function(book){
-//   console.log(book);
-// });
+Array.from(books).forEach(function(book){
+  console.log(book);
+});
