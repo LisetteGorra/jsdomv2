764e0884fb9a68fa2df7260bf7bd038eb1ba45b9
Lisette Gorra <lisettegorra.student@careerdevs.com>
2018-12-19 00:29:52 +0000

    Used .parentNode, .parentElement, .parentElement.parentElement, .children

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const banner = document.querySelector('#page-banner');
-console.log('#page-banner node type is:',banner.nodeType);
-console.log('#page-banner node name is:',banner.nodeName);
-console.log('#page-banner has child nodes:',banner.hasChildNodes());
-const clonedBanner = banner.cloneNode(true);
-console.log(clonedBanner);
+const bookList = document.querySelector('#book-list');
+
+console.log('the parent node is:', bookList.parentNode);
+console.log('the parent element is:', bookList.parentElement.parentElement);
+console.log(bookList.children);
 
 
 
@@ -22,12 +21,24 @@ console.log(clonedBanner);
 
 
 
+// const bookList = document.querySelector('#book-list');
 
-// const banner = document.querySelector('#page-banner');
+// console.log('book list parent element:', bookList.parentElement);
+// console.log('book list parent node:', bookList.parentNode);
 
-// console.log('#page-banner node type is:', banner.nodeType);
-// console.log('#page-banner node name is:', banner.nodeName);
-// console.log('#page-banner has child nodes:', banner.hasChildNodes());
+// console.log('all node children:');
+// Array.from(bookList.childNodes).forEach(function(node){
+//   console.log(node);
+// });
 
-// const clonedBanner = banner.cloneNode(true);
-// console.log(clonedBanner);
+// console.log('all element children:');
+// Array.from(bookList.children).forEach(function(node){
+//   console.log(node);
+// });
+
+// const titles = bookList.querySelectorAll('.name');
+
+// console.log('Book titles:');
+// Array.from(titles).forEach(function(title){
+//   console.log(title.textContent);
+// });
