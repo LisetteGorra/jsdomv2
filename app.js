bb2e6b09dab6699b7050e22d9ac2092a5092ed45
Lisette Gorra <lisettegorra.student@careerdevs.com>
2018-12-20 23:43:54 +0000

    Used addForm, .forms, e.preventDefault, input[type=text], .value

--- app.js
+++ app.js
@@ -1,39 +1,19 @@
 const list = document.querySelector('#book-list ul');
 
 
-//delete books
-list.addEventListener('click', function(e){
-   if(e.target.className == 'delete'){
-       const li = e.target.parentElement;  
-       list.removeChild(li); 
-   }
+list.addEventListener('click', (e) => {
+    if (e.target.className == 'delete') {
+        const li = e.target.parentElement;
+        li.parentNode.removeChild(li);
+    }
 });
 
 
+const addForm = document.forms['add-book'];
 
+addForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+    console.log(value);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const list = document.querySelector('#book-list ul');
-
-// // delete books
-// list.addEventListener('click', (e) => {
-//   if(e.target.className == 'delete'){
-//     const li = e.target.parentElement;
-//     li.parentNode.removeChild(li);
-//   }
-// });
+});
