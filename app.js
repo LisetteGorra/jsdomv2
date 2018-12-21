337b3a30a88628de1f6c4d894711de2f5fcc0112
Lisette Gorra <lisettegorra.student@careerdevs.com>
2018-12-21 00:45:34 +0000

    Used .textContent, deleteBtn, value, delete, span, .createElement, li.appendChild, ppendChild

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const list = document.querySelector('#book-list ul');
+const forms = document.forms;
 
-
+// delete books
 list.addEventListener('click', (e) => {
     if (e.target.className == 'delete') {
         const li = e.target.parentElement;
@@ -8,12 +9,67 @@ list.addEventListener('click', (e) => {
     }
 });
 
-
-const addForm = document.forms['add-book'];
-
+// add books
+const addForm = forms['add-book'];
 addForm.addEventListener('submit', function(e) {
     e.preventDefault();
-    const value = addForm.querySelector('input[type="text"]').value;
-    console.log(value);
-
+ const value = addForm.querySelector('input[type="text"]').value;
+   
+   
+    //create elements
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+    
+    //add content
+    deleteBtn.textContent = 'delete';
+    bookName.textContent = value;
+    
+  
+   
+    //append to DOM
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+     list.appendChild(li);
+    
+    
 });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+//  const li = document.createElement('li');
+//   const bookName = document.createElement('span');
+//   const deleteBtn = document.createElement('span');
+
+//   // add text content
+//   bookName.textContent = value;
+//   deleteBtn.textContent = 'delete';
+
+//   // append to DOM
+//   li.appendChild(bookName);
+//   li.appendChild(deleteBtn);
+//   list.appendChild(li);
+//   //list.insertBefore(li, list.querySelector('li:first-child'));
+// });
