1e31fec90648fcf041ea6ec28ae91e9998fc2b4f
Lisette Gorra <lisettegorra.student@careerdevs.com>
2018-12-21 01:44:08 +0000

    Used .style, .display, initial, change, hideBox, .checked, if else statements, we made all the books dispear when hideBox was checked

--- app.js
+++ app.js
@@ -33,3 +33,14 @@ addForm.addEventListener('submit', function(e){
   li.appendChild(deleteBtn);
   list.appendChild(li);
 });
+
+//hide books
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change', function(e){
+  if(hideBox.checked){
+    list.style.display = "none";
+  }
+  else {
+    list.style.display = "initial";
+  }
+});
